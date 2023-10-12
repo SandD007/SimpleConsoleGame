f0eeb52a33e6a3c3ba9733fcd637c1a4f0d630e8
Dmytro Kulinich <spo1091@i.ua>
2023-10-12 17:35:13 +0300

    Add proj

---
+++ BigAndSmall/BigAndSmall/HiloGame.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigAndSmall
+{
+    public static class HiloGame
+    {
+        public const int MAXIMUM = 100;
+        static Random random = new Random();
+        private static int currentNumber  = random.Next(1,MAXIMUM +1 );
+        private static int newNumber = random.Next(1, MAXIMUM + 1);
+        private static int pot = 11;
+
+        public static void Guess(bool higer)
+        {
+            Console.WriteLine("\n");
+            newNumber = random.Next(1,MAXIMUM +1);
+            if (higer)
+            {
+                if(newNumber >= currentNumber)
+                {
+                    Console.WriteLine("Ти вгадав!");
+                    pot++;
+                }
+                else
+                {
+                    Console.WriteLine("Ох, тобi не пощастило!");
+                    pot--;
+                }
+            }
+            else
+            {
+                if (newNumber <= currentNumber)
+                {
+                    Console.WriteLine("Ти вгадав!");
+                    pot++;
+                }
+                else
+                {
+                    Console.WriteLine("Ох, тобi не пощастило!");
+                    pot--;
+                }
+            }
+            currentNumber = newNumber;
+            Console.WriteLine($"Число : {currentNumber}");
+        }
+
+        public static void Hint()
+        {
+            Console.WriteLine("\n");
+            int half = MAXIMUM / 2;
+            if (newNumber <= half)
+            {
+                Console.WriteLine($"Це число менше за {half} !");
+                pot--;
+            }
+            else
+            {
+                Console.WriteLine($"Це число бiльше {half}");
+                pot--;
+            }
+        }
+
+        public static int GetPot()
+        {
+            return pot;
+        }
+    }
+}
---
+++ BigAndSmall/BigAndSmall/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BigAndSmall;
+
+Console.WriteLine("Вiтаю в ХIЛО.");
+Console.WriteLine($"Випадкове число може бути вiв 1 до {HiloGame.MAXIMUM}.");
+HiloGame.Hint();
+while (HiloGame.GetPot() > 0)
+{
+    Console.WriteLine("Натисни H для вибору бiльшого числа, L для меншого, W щоб купити пiдказку,");
+    Console.WriteLine($"або будь яку клавiшу для виходу з {HiloGame.GetPot()} гривнями в гаманцi");
+
+    char key = Console.ReadKey(true).KeyChar;
+    if (key == 'h' || key == 'H')
+    {
+        Console.Clear();
+        HiloGame.Guess(true);
+    }
+    else if (key == 'l' || key == 'L')
+    {
+        Console.Clear();
+        HiloGame.Guess(false);
+    }
+    else if (key == 'w' || key == 'W')
+    {
+        Console.Clear();
+        HiloGame.Hint();
+    }
+    else return;
+}
+Console.Clear();
+Console.WriteLine("ТИ ПРОГРАВ! ДО ЗУСТРIЧI!");
